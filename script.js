7fcd4fb2d7a2c6c078969cd77ed04176b33493f7
fanoyrios <[aeroporoy10@hotmail.com]>
2020-12-30 02:55:00 +0200

    adding a bunch of files

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("bookcase_modal");
+var btn = document.getElementById("house_modal");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
